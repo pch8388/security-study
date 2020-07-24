a9508f75396121a0024b189fc584894bbcb918ce
pch8388 <pch838811@gmail.com>
2020-07-24 22:42:52 +0900

    form 로그인을 위한 준비

---
+++ src/main/java/me/study/securitystudy/form/SampleController.java
@@ -0,0 +1,41 @@
+package me.study.securitystudy.form;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/")
+    public String index(Model model, Principal principal) {
+        Optional.ofNullable(principal)
+            .ifPresentOrElse(p -> model.addAttribute("message", "Hello, " + p.getName()),
+                () -> model.addAttribute("message", "Hello Spring Security"));
+
+        return "index";
+    }
+
+    @GetMapping("/info")
+    public String info(Model model) {
+        model.addAttribute("message", "info");
+        return "info";
+    }
+
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute("message", "Hello " + principal.getName());
+        return "dashboard";
+    }
+
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal) {
+        model.addAttribute("message", "Hello Admin, " + principal.getName());
+        return "admin";
+    }
+}
